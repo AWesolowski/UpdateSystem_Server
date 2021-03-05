012444f197230dda67c892ec6495bcec7af88c54
Adam <awesolow@mion.elka.pw.edu.pl>
2021-03-05 05:44:37 +0100

    Connection working

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,30 @@
-
-
-
-
-
-
-
-
-
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server
 {
 
+    static final int PORT = 1234;
+
+
+
+
+
+    public static void main(String[] args)
+    {
+        ServerSocket serverSocket;
+        Socket socket;
+
+        try
+        {
+            serverSocket = new ServerSocket(PORT);
+            socket = serverSocket.accept();
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
 
 
@@ -22,6 +35,18 @@ public class Server
 
 
 
+            // close everything
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
 
+            System.out.println("bye bye");
+        }
+        catch (IOException e)
+        {
+            System.err.println("Server exeption : " + e);
+        }
 
+    }
 }
